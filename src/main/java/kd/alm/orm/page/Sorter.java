871fc7971563b490abbc1c0252d2924a918d4d8f
Chen Quan <chenquan.dev@foxmail.com>
2021-05-22 03:16:23 +0000

    Fix order bug

--- src/main/java/kd/alm/orm/page/Sorter.java
+++ src/main/java/kd/alm/orm/page/Sorter.java
@@ -1,19 +1,16 @@
 package kd.alm.orm.page;
 
-import lombok.Data;
 import lombok.ToString;
-
-import java.io.Serializable;
 
 /**
  * </p>
- * description
+ * 顺序
  * <p>
  *
  * @author chenquan chenquan.dev@foxmail.com 2021/05/10 11:54
  */
 @ToString
-public enum Sorter implements Serializable {
+public enum Sorter {
     /**
      * 降序
      */
